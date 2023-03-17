bd3100211e5d54821580bb83d177f397d9e54f95
Ray Anthoney <rmaxey911@gmail.com>
2023-03-17 07:06:20 -0600

    added the property controller

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import property from "../mongodb/models/property.js";
+import Property from "../mongodb/models/property.js";
 import User from "../mongodb/models/user.js";
 
 import * as dotenv from "dotenv";
@@ -14,8 +14,35 @@ cloudinary.config({
 });
 
 const getAllProperties = async (req, res) => {
+  const {
+    _end,
+    _order,
+    _start,
+    _sort,
+    title_like = "",
+    propertyType = "",
+  } = req.query;
+
+  const query = {};
+
+  if (propertyType !== "") {
+    query.propertyType = propertyType;
+  }
+
+  if (title_like) {
+    query.title = { $regex: title_like, $options: "i" };
+  }
+
   try {
-    const properties = await property.find({}).limit(req.query._end );
+    const count = await Property.countDocuments({ query });
+
+    const properties = await Property.find(query)
+      .limit(_end)
+      .skip(_start)
+      .sort({ [_sort]: _order });
+
+    res.header("x-total-count", count);
+    res.header("Access-Control-Expose-Headers", "x-total-count");
 
     res.status(200).json(properties);
   } catch (error) {
