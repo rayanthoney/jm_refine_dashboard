e303c89952c971964698f3a63de8d69a49f220d4
Ray Anthoney <rmaxey911@gmail.com>
2023-03-17 07:07:35 -0600

    added the to the all-properties

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,51 +1,163 @@
+import { Add } from "@mui/icons-material";
+import { useTable } from "@pankod/refine-core";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@pankod/refine-mui";
+import { useNavigate } from "@pankod/refine-react-router-v6";
 
-import { Add } from '@mui/icons-material';
-import { useTable } from '@pankod/refine-core';
-import { Box, Stack, Typography } from '@pankod/refine-mui';
-import { useNavigate } from '@pankod/refine-react-router-v6';
-
-import { PropertyCard, CustomButton } from 'components';
+import { PropertyCard, CustomButton } from "components";
 
 const AllProperties = () => {
   const navigate = useNavigate();
 
   const {
-    tableQueryResult: { data, isLoading, isError }
+    tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter,
+    setSorter,
+    filters,
+    setFilters,
   } = useTable();
 
-  const AllProperties = data?.data ?? [];
+  const allProperties = data?.data ?? [];
 
-  if(isLoading) return <Typography>Loading...</Typography>
-  if(isError) return <Typography>Error...</Typography>
-  
+  const currentPrice = sorter.find((item) => item.field === "price")?.order;
+
+  const toggleSort = (field: string) => {
+    setSorter([{ field, order: currentPrice === "asc" ? "desc" : "asc" }]);
+  };
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
+
   return (
     <Box>
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        <Stack direction="column" width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142d">
+            {!allProperties.length
+              ? "There are no properties"
+              : "All Properties"}
+          </Typography>
+          <Box
+            mb={2}
+            mt={3}
+            display="flex"
+            width="84%"
+            justifyContent="space-between"
+            flexWrap="wrap"
+          >
+            <Box
+              display="flex"
+              gap={2}
+              flexWrap="wrap"
+              mb={{ xs: "20px", sm: 0 }}
+            >
+              <CustomButton
+                title={`Sort price ${currentPrice === "asc" ? "↑" : "↓"}`}
+                handleClick={() => toggleSort("price")}
+                backgroundColor="#475be8"
+                color="#fcfcfc"
+              />
+              <TextField
+                variant="outlined"
+                color="info"
+                placeholder="Search by title"
+                value=""
+                onChange={() => {}}
+              />
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue=""
+                value=""
+                onChange={() => {}}
+              >
+                <MenuItem value="">All</MenuItem>
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+
       <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography fontSize={25} fontWeight={700} color='#11142d'>All Properties</Typography>
-        <CustomButton 
+        <CustomButton
           title="Add Property"
-          handleClick={() => navigate('/properties/create')}
+          handleClick={() => navigate("/properties/create")}
           backgroundColor="#475be8"
           color="#fcfcfc"
           icon={<Add />}
         />
       </Stack>
-      <Box mt="20px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 3 }} >
-        {AllProperties.map((property) => (
-          <PropertyCard 
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties.map((property) => (
+          <PropertyCard
             key={property._id}
             id={property._id}
-            title={property._title}
-            price={property._price}
-            location={property._location}
-            photo={property._photo}
-
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
           />
         ))}
-
       </Box>
-    </Box>
-  )
-}
 
-export default AllProperties
+      {allProperties.length > 0 && (
+        <Box display="flex" gap={2} mt={3} flexWrap="wrap">
+          <CustomButton
+            title="Previous"
+            handleClick={() => setCurrent((prev) => prev - 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={!(current > 1)}
+          />
+          <Box
+            display={{ xs: "hidden", sm: "flex" }}
+            alignItems="center"
+            gap="5px"
+          >
+            Page{" "}
+            <strong>
+              {current} of {pageCount}{" "}
+            </strong>
+          </Box>
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent((prev) => prev + 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={!(current === pageCount)}
+          />
+          <Select
+            variant="outlined"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{ "aria-label": "Without label" }}
+            defaultValue={10}
+            onChange={() => {}}
+          >
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>
+                Show{size}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default AllProperties;
